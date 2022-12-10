fdbb28c1bdde466cdc9d57f4015c568c0cc932b9
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2022-12-10 20:33:47 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -275,7 +275,7 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
             # Clear previously computed gradients
             self._optimizer.zero_grad()
 
-            loss = self._loss_function(self._model, batch, self.URM_train)
+            loss = self._loss_function(self._model, batch)
 
             # Compute gradients given current loss
             loss.backward()
