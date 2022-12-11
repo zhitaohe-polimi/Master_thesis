3e7b9fa87e7d1e67f50785b985f368fbd8be2d03
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2022-12-11 12:07:57 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -59,7 +59,9 @@ class _SimpleNewMFModel(torch.nn.Module):
 
         user_sim = torch.einsum("bi,ci->bc", URM[user], URM)
         print(user_sim.shape)
-        total_user = list(range(n_user))
+        user_list = list(range(n_user))
+        total_user = torch.Tensor([user_list]).type(torch.LongTensor)
+        print(total_user.shape,item.shape)
         print(self._embedding_user_u(total_user).shape,self._embedding_item_u(item).shape)
         # MF_u = batch_dot(self._embedding_user_u(total_user), self._embedding_item_u(item))
         # prediction += torch.einsum("ik,jk->ij", user_sim, MF_u)
