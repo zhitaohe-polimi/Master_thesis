546f12dd685e02d18baee986fd27201fc61bd9dc
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2022-12-15 12:40:15 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -290,7 +290,9 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
         ITEM_factors_u = torch.tensor(self.ITEM_factors_u).to("cuda")
         USER_factors_i = torch.tensor(self.USER_factors_i).to("cuda")
         ITEM_factors_i = torch.tensor(self.ITEM_factors_i).to("cuda")
+        print(user_id_array.shape)
         user_id_array = torch.Tensor(user_id_array).type(torch.LongTensor).to("cuda")
+        print(user_id_array.shape)
 
         if items_to_compute is not None:
             item_scores = - np.ones((len(user_id_array), self.ITEM_factors.shape[0]), dtype=np.float32) * np.inf
