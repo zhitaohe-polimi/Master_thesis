052131c768026fb77fadf480c78a070229dc3b13
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-01-04 19:55:37 +0100

    updated

--- Recommenders/MatrixFactorization/PyTorchMF.py
+++ Recommenders/MatrixFactorization/PyTorchMF.py
@@ -58,7 +58,6 @@ class _SimpleMFBiasModel(torch.nn.Module):
         prediction = self._global_bias + self._user_bias[user] + self._item_bias[item]
         prediction += batch_dot(self._embedding_user(user), self._embedding_item(item))
 
-        print(torch.einsum("bi,bi->bb", self._embedding_user(user), self._embedding_item(item)).shape)
         return prediction
 
 
