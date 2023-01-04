554d2882a520e39f5e5a2e94b97bdf5e052cfbbc
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-01-04 15:53:43 +0100

    Update run_hyperparameter_search.py

--- HyperparameterTuning/run_hyperparameter_search.py
+++ HyperparameterTuning/run_hyperparameter_search.py
@@ -869,7 +869,7 @@ def runHyperparameterSearch_Collaborative(recommender_class, URM_train, URM_trai
         if recommender_class is PyTorchMF_BPR_Recommender or recommender_class is PyTorchMF_MSE_Recommender:
             hyperparameters_range_dictionary = {
                 "num_factors": Integer(1, 200),
-                "epochs": Categorical([300]),
+                "epochs": Categorical([1500]),
                 "sgd_mode": Categorical(["sgd", "adagrad", "adam", "rmsprop"]),
                 "batch_size": Categorical([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]),
                 "learning_rate": Real(low=1e-4, high=1e-1, prior='log-uniform'),
