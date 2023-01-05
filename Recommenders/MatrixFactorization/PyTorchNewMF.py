05412ea597e4a8d4122501b7b5e9205cf4d148c7
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-01-05 16:00:01 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -351,17 +351,17 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
         user_list = list(range(self.n_users))
         self.all_users = torch.Tensor(user_list).type(torch.LongTensor)#.to(device)
         self.users_sim = torch.einsum("bi,ci->bc", self.URM_tensor, self.URM_tensor)
-        self.users_sim = torch.nn.functional.normalize(self.users_sim, dim=1)
         # set all elements in diagnal to 0
         self.users_sim = self.users_sim.fill_diagonal_(0)
+        self.users_sim = torch.nn.functional.normalize(self.users_sim, dim=1)
         # self.users_sim = self.users_sim.to(device)
 
         item_list = list(range(self.n_items))
         self.all_items = torch.Tensor(item_list).type(torch.LongTensor)#.to(device)
         self.items_sim = torch.einsum("ib,ic->bc", self.URM_tensor, self.URM_tensor)
-        self.items_sim = torch.nn.functional.normalize(self.items_sim, dim=1)
         # set all elements in diagnal to 0
         self.items_sim = self.items_sim.fill_diagonal_(0)
+        self.items_sim = torch.nn.functional.normalize(self.items_sim, dim=1)
         # self.items_sim = self.items_sim.to(device)
 
         if sgd_mode.lower() == "adagrad":
