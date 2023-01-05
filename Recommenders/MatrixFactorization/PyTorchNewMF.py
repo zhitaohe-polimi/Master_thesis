db70fdb7da94f38be651688120132ab853b337d6
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-01-05 20:12:23 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -446,6 +446,8 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
 
             epoch_loss += loss.item()
 
+        print("%s: epoch_loss: %.2f" % (self.RECOMMENDER_NAME,epoch_loss))
+
 
 class PyTorchNewMF_BPR_Recommender(_PyTorchMFRecommender):
     RECOMMENDER_NAME = "PyTorchNewMF_BPR_Recommender"
