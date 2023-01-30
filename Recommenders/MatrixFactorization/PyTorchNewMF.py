0866f8c6e79c51ce3b93c1d0c48a4fc56a71dff5
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-01-30 18:49:06 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -358,24 +358,24 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
         self.ITEM_factors_uj = self.ITEM_factors_best_uj.copy()
 
     def _prepare_model_for_validation(self):
-        self.USER_factors = self._model._embedding_user.weight.detach().cpu().numpy()
-        self.ITEM_factors = self._model._embedding_item.weight.detach().cpu().numpy()
+        self.USER_factors = self._model._embedding_user#.weight.detach().cpu().numpy()
+        self.ITEM_factors = self._model._embedding_item#.weight.detach().cpu().numpy()
 
-        self.USER_factors_vi = self._model._embedding_user_vi.weight.detach().cpu().numpy()
-        self.ITEM_factors_vi = self._model._embedding_item_vi.weight.detach().cpu().numpy()
+        self.USER_factors_vi = self._model._embedding_user_vi#.weight.detach().cpu().numpy()
+        self.ITEM_factors_vi = self._model._embedding_item_vi#.weight.detach().cpu().numpy()
 
-        self.USER_factors_uj = self._model._embedding_user_uj.weight.detach().cpu().numpy()
-        self.ITEM_factors_uj = self._model._embedding_item_uj.weight.detach().cpu().numpy()
+        self.USER_factors_uj = self._model._embedding_user_uj#.weight.detach().cpu().numpy()
+        self.ITEM_factors_uj = self._model._embedding_item_uj#.weight.detach().cpu().numpy()
 
     def _update_best_model(self):
-        self.USER_factors_best = self._model._embedding_user.weight.detach().cpu().numpy()
-        self.ITEM_factors_best = self._model._embedding_item.weight.detach().cpu().numpy()
+        self.USER_factors_best = self._model._embedding_user#.weight.detach().cpu().numpy()
+        self.ITEM_factors_best = self._model._embedding_item#.weight.detach().cpu().numpy()
 
-        self.USER_factors_best_vi = self._model._embedding_user_vi.weight.detach().cpu().numpy()
-        self.ITEM_factors_best_vi = self._model._embedding_item_vi.weight.detach().cpu().numpy()
+        self.USER_factors_best_vi = self._model._embedding_user_vi#.weight.detach().cpu().numpy()
+        self.ITEM_factors_best_vi = self._model._embedding_item_vi#.weight.detach().cpu().numpy()
 
-        self.USER_factors_best_uj = self._model._embedding_user_uj.weight.detach().cpu().numpy()
-        self.ITEM_factors_best_uj = self._model._embedding_item_uj.weight.detach().cpu().numpy()
+        self.USER_factors_best_uj = self._model._embedding_user_uj#.weight.detach().cpu().numpy()
+        self.ITEM_factors_best_uj = self._model._embedding_item_uj#.weight.detach().cpu().numpy()
 
     def _run_epoch(self, num_epoch):
 
