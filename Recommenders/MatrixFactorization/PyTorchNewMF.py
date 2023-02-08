100b4b7499a9a316c813ff3a6d9b95e70b3947e2
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-02-08 20:52:19 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -85,6 +85,7 @@ class _SimpleNewMFModel(torch.nn.Module):
         user_sim_uv = torch.einsum("bi,ci->bc", self._embedding_user(user), self._embedding_user.weight)
         user_sim_uv[:, user] = user_sim_uv[:, user].fill_diagonal_(0)
         user_sim_uv = torch.nn.functional.normalize(user_sim_uv, dim=1)
+        print(user_sim_uv)
         alpha_vi = torch.einsum("bi,ci->bc", self._embedding_user_vi.weight, self._embedding_item_vi(item))
         summation_v = torch.einsum("bi,ib->b", user_sim_uv, alpha_vi)
         prediction += summation_v
@@ -95,9 +96,6 @@ class _SimpleNewMFModel(torch.nn.Module):
         alpha_uj = torch.einsum("bi,ci->bc", self._embedding_user_uj(user), self._embedding_item_uj.weight)
         summation_j = torch.einsum("bi,ib->b", alpha_uj, item_sim_ij)
         prediction += summation_j
-
-
-        print(prediction)
 
         return prediction
 
