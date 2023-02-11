e7f3242af07eb2c78a047e4392405ed2c17258c6
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-02-11 19:17:56 +0100

    Update PyTorchMF.py

--- Recommenders/MatrixFactorization/PyTorchMF.py
+++ Recommenders/MatrixFactorization/PyTorchMF.py
@@ -293,7 +293,7 @@ class PyTorchMF_BPR_Recommender(_PyTorchMFRecommender):
 
 
 class PyTorchMF_MSE_Recommender(_PyTorchMFRecommender):
-    RECOMMENDER_NAME = "PyTorchMF_MSE_Recommender"
+    RECOMMENDER_NAME = "PyTorchMF_MSE_Recommender_test"
 
     def __init__(self, URM_train, verbose=True):
         super(PyTorchMF_MSE_Recommender, self).__init__(URM_train, verbose=verbose)
