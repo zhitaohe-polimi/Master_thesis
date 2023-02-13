2da0182f04e5b78fbc1c85685c739d22fb8845ad
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-02-13 12:37:25 +0100

    Update run_hyperparameter_search.py

--- HyperparameterTuning/run_hyperparameter_search.py
+++ HyperparameterTuning/run_hyperparameter_search.py
@@ -850,7 +850,7 @@ def runHyperparameterSearch_Collaborative(recommender_class, URM_train, URM_trai
                 "num_factors_i": Integer(1, 200),
                 "epochs": Categorical([1500]),
                 "sgd_mode": Categorical(["sgd", "adagrad", "adam", "rmsprop"]),
-                "batch_size": Categorical([128, 256, 512, 1024]), #
+                "batch_size": Categorical([128, 256, 512, 1024]),
                 "learning_rate": Real(low=1e-4, high=1e-1, prior='log-uniform'),
                 "l2_reg": Real(low=1e-5, high=1e-2, prior='log-uniform'),
             }
@@ -914,7 +914,7 @@ def runHyperparameterSearch_Collaborative(recommender_class, URM_train, URM_trai
                 "num_factors": Integer(1, 200),
                 "epochs": Categorical([1500]),
                 "sgd_mode": Categorical(["sgd", "adagrad", "adam", "rmsprop"]),
-                "batch_size": Categorical([ 1024]), #16, 32, 64, 128, 256, 512,
+                "batch_size": Categorical([16, 32, 64, 128, 256, 512, 1024]),
                 "learning_rate": Real(low=1e-4, high=1e-1, prior='log-uniform'),
                 "l2_reg": Real(low=1e-5, high=1e-2, prior='log-uniform'),
             }
