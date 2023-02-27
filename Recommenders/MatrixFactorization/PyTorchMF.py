57e5d825fd5736488e5b492a4e48a2fba4cabb9c
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-02-27 18:14:07 +0100

    Update PyTorchMF.py

--- Recommenders/MatrixFactorization/PyTorchMF.py
+++ Recommenders/MatrixFactorization/PyTorchMF.py
@@ -280,8 +280,7 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
             # Clear previously computed gradients
             self._optimizer.zero_grad()
 
-            batch = np.array(batch)
-            print(batch)
+            batch = np.array(batch).astype(int)
             batch = torch.from_numpy(batch).to("cuda")
             user, item_positive, item_negative = batch
 
