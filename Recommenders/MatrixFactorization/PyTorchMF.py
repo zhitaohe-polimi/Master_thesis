196034c63ba553c1de0d46e9a918c62e71208d27
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-02-28 23:15:24 +0100

    Update PyTorchMF.py

--- Recommenders/MatrixFactorization/PyTorchMF.py
+++ Recommenders/MatrixFactorization/PyTorchMF.py
@@ -212,8 +212,8 @@ def loss_BPR(model, batch, l2_reg):
 
     # Compute total loss for batch
     BPR_loss = -x_ij.sigmoid().log().mean()
-    # BPR_loss +
-    loss =  reg_loss * l2_reg
+
+    loss = BPR_loss + reg_loss * l2_reg
 
     return loss
 
