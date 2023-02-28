7e0f7aebf21a0ed1c357b048e89f12ac9044bbba
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-01 00:26:52 +0100

    Update run_hyperparameter_search.py

--- HyperparameterTuning/run_hyperparameter_search.py
+++ HyperparameterTuning/run_hyperparameter_search.py
@@ -571,7 +571,7 @@ def runHyperparameterSearch_Collaborative(recommender_class, URM_train, URM_trai
     earlystopping_keywargs = {"validation_every_n": 5,
                               "stop_on_validation": True,
                               "evaluator_object": evaluator_validation_earlystopping,
-                              "lower_validations_allowed": 5,
+                              "lower_validations_allowed": 10,
                               "validation_metric": metric_to_optimize,
                               }
 
@@ -893,7 +893,7 @@ def runHyperparameterSearch_Collaborative(recommender_class, URM_train, URM_trai
                 "num_factors": Integer(1, 200),
                 "epochs": Categorical([1500]),
                 "sgd_mode": Categorical(["sgd", "adagrad", "adam", "rmsprop"]),
-                "batch_size": Categorical([16, 32, 64, 128, 256, 512, 1024]),  # 1, 2, 4, 8, 16, 32,
+                "batch_size": Categorical([64, 128, 256, 512, 1024]),  # 1, 2, 4, 8, 16, 32,
                 "learning_rate": Real(low=1e-4, high=1e-1, prior='log-uniform'),
                 "l2_reg": Real(low=1e-5, high=1e-2, prior='log-uniform'),
                 "positive_quota": Real(low=0.5, high=1.0, prior='uniform'),
