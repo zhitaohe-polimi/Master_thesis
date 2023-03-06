5675183d56919d143e5ed6f3adbac1cf97692e0d
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-06 14:41:02 +0100

    Update PyTorchMF.py

--- Recommenders/MatrixFactorization/PyTorchMF.py
+++ Recommenders/MatrixFactorization/PyTorchMF.py
@@ -280,15 +280,15 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
         epoch_loss = 0
 
         for batch in self._data_iterator:
-            # Clear previously computed gradients
-            self._optimizer.zero_grad()
-
             user, item, rating = batch
             user = user.to("cuda")
             item = item.to("cuda")
             rating = rating.to("cuda")
 
             batch = (user, item, rating)
+
+            # Clear previously computed gradients
+            self._optimizer.zero_grad()
 
             loss = self._loss_function(self._model, batch)
 
