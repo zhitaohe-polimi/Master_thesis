ed5affe37d8b1bf4c9ae40162d7491fca882aecd
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-07 14:55:43 +0100

    Update PyTorchMF.py

--- Recommenders/MatrixFactorization/PyTorchMF.py
+++ Recommenders/MatrixFactorization/PyTorchMF.py
@@ -6,6 +6,7 @@ Created on 07/08/2022
 @author: Maurizio Ferrari Dacrema
 """
 
+
 from Recommenders.BaseMatrixFactorizationRecommender import BaseMatrixFactorizationRecommender
 from Recommenders.Incremental_Training_Early_Stopping import Incremental_Training_Early_Stopping
 import scipy.sparse as sps
@@ -13,12 +14,6 @@ import numpy as np
 import torch, os
 from torch.utils.data import Dataset, DataLoader
 from torch.profiler import profile, record_function, ProfilerActivity
-from Utils.PyTorch.Cython.DataIterator import BPRIterator as BPRIterator_cython, \
-    InteractionIterator as InteractionIterator_cython, \
-    InteractionAndNegativeIterator as InteractionAndNegativeIterator_cython
-from Utils.PyTorch.DataIterator import BPRIterator, InteractionIterator, InteractionAndNegativeIterator
-
-torch.autograd.set_detect_anomaly(True)
 
 
 def batch_dot(tensor_1, tensor_2):
@@ -37,25 +32,12 @@ class _SimpleMFModel(torch.nn.Module):
     def __init__(self, n_users, n_items, embedding_dim=20):
         super(_SimpleMFModel, self).__init__()
 
-        self._embedding_user = torch.nn.Embedding(n_users, embedding_dim=embedding_dim)
-        self._embedding_item = torch.nn.Embedding(n_items, embedding_dim=embedding_dim)
+        self._embedding_user = torch.nn.Embedding(n_users, embedding_dim = embedding_dim)
+        self._embedding_item = torch.nn.Embedding(n_items, embedding_dim = embedding_dim)
 
     def forward(self, user, item):
         prediction = batch_dot(self._embedding_user(user), self._embedding_item(item))
         return prediction
-
-    def reg_loss(self, user, item):
-        reg_loss = (1 / 2) * (self._embedding_user(user).pow(2).sum() +
-                              self._embedding_item(item).pow(2).sum()) / float(len(user))
-
-        return reg_loss
-
-    def reg_loss_bpr(self, user, item, rating):
-        reg_loss = (1 / 2) * (self._embedding_user(user).norm(2).pow(2) +
-                              self._embedding_item(item).norm(2).pow(2) +
-                              self._embedding_item(rating).norm(2).pow(2)) / float(len(user))
-
-        return reg_loss
 
 
 class _SimpleMFBiasModel(torch.nn.Module):
@@ -63,11 +45,12 @@ class _SimpleMFBiasModel(torch.nn.Module):
     def __init__(self, n_users, n_items, embedding_dim=20):
         super(_SimpleMFBiasModel, self).__init__()
 
-        self._embedding_user = torch.nn.Embedding(n_users, embedding_dim=embedding_dim)
-        self._embedding_item = torch.nn.Embedding(n_items, embedding_dim=embedding_dim)
+        self._embedding_user = torch.nn.Embedding(n_users, embedding_dim = embedding_dim)
+        self._embedding_item = torch.nn.Embedding(n_items, embedding_dim = embedding_dim)
         self._global_bias = torch.nn.Parameter(torch.randn((1), dtype=torch.float))
         self._user_bias = torch.nn.Parameter(torch.randn((n_users), dtype=torch.float))
         self._item_bias = torch.nn.Parameter(torch.randn((n_items), dtype=torch.float))
+
 
     def forward(self, user, item):
         prediction = self._global_bias + self._user_bias[user] + self._item_bias[item]
@@ -80,12 +63,14 @@ class _SimpleAsySVDModel(torch.nn.Module):
     def __init__(self, n_users, n_items, embedding_dim=20):
         super(_SimpleAsySVDModel, self).__init__()
 
-        self._embedding_user_profile = torch.nn.Embedding(n_items, embedding_dim=embedding_dim)
-        self._embedding_item = torch.nn.Embedding(n_items, embedding_dim=embedding_dim)
+        self._embedding_user_profile = torch.nn.Embedding(n_items, embedding_dim = embedding_dim)
+        self._embedding_item = torch.nn.Embedding(n_items, embedding_dim = embedding_dim)
 
     def forward(self, user, item, user_profile):
         prediction = batch_dot(self._embedding_user(user), self._embedding_item(item))
         return prediction
+
+
 
 
 class URM_Dataset(Dataset):
@@ -102,6 +87,7 @@ class URM_Dataset(Dataset):
 
     def __getitem__(self, index):
         return self._row[index], self._col[index], self._data[index]
+
 
 
 class Interaction_Dataset(Dataset):
@@ -127,7 +113,7 @@ class Interaction_Dataset(Dataset):
             return self._row[index], self._col[index], self._data[index]
         else:
             user_id = self._row[index]
-            seen_items = self._URM_train.indices[self._URM_train.indptr[user_id]:self._URM_train.indptr[user_id + 1]]
+            seen_items = self._URM_train.indices[self._URM_train.indptr[user_id]:self._URM_train.indptr[user_id+1]]
             negative_selected = False
 
             while not negative_selected:
@@ -151,7 +137,7 @@ class BPR_Dataset(Dataset):
 
     def __getitem__(self, user_id):
 
-        seen_items = self._URM_train.indices[self._URM_train.indptr[user_id]:self._URM_train.indptr[user_id + 1]]
+        seen_items = self._URM_train.indices[self._URM_train.indptr[user_id]:self._URM_train.indptr[user_id+1]]
         item_positive = np.random.choice(seen_items)
 
         # seen_items = set(list(seen_items))
@@ -168,24 +154,17 @@ class BPR_Dataset(Dataset):
         return user_id, item_positive, item_negative
 
 
-def loss_MSE(model, batch, l2_reg):
+
+def loss_MSE(model, batch):
     user, item, rating = batch
-    user = user.to("cuda")
-    item = item.to("cuda")
-    rating = rating.to("cuda")
 
     # Compute prediction for each element in batch
     prediction = model.forward(user, item)
 
     # Compute total loss for batch
-    MSE_loss = (prediction - rating).pow(2).mean()
-
-    reg_loss = model.reg_loss(user, item)
-
-    loss = MSE_loss + reg_loss * l2_reg
+    loss = (prediction - rating).pow(2).mean()
 
     return loss
-
 
 def loss_CrossEntropy(model, batch):
     user, item, ground_truth = batch
@@ -200,21 +179,14 @@ def loss_CrossEntropy(model, batch):
     return loss
 
 
-def loss_BPR(model, batch, l2_reg):
+def loss_BPR(model, batch):
     user, item_positive, item_negative = batch
-    user = user.to("cuda")
-    item_positive = item_positive.type(torch.long).to("cuda")
-    item_negative = item_negative.type(torch.long).to("cuda")
 
     # Compute prediction for each element in batch
     x_ij = model.forward(user, item_positive) - model.forward(user, item_negative)
 
     # Compute total loss for batch
-    BPR_loss = -(x_ij.sigmoid() + 1e-20).log().mean()
-
-    reg_loss = model.reg_loss_bpr(user, item_positive, item_negative)
-
-    loss = BPR_loss + reg_loss * l2_reg
+    loss = -x_ij.sigmoid().log().mean()
 
     return loss
 
@@ -225,51 +197,39 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
 
     RECOMMENDER_NAME = "_PyTorchMFRecommender"
 
-    def __init__(self, URM_train, verbose=True):
-        super(_PyTorchMFRecommender, self).__init__(URM_train, verbose=verbose)
+    def __init__(self, URM_train, verbose = True):
+        super(_PyTorchMFRecommender, self).__init__(URM_train, verbose = verbose)
 
     def fit(self, epochs=300,
-            batch_size=8,
-            num_factors=32,
-            l2_reg=1e-4,
-            sgd_mode='adam',
-            learning_rate=1e-2,
+            batch_size = 8,
+            num_factors = 32,
+            l2_reg = 1e-4,
+            sgd_mode = 'adam',
+            learning_rate = 1e-2,
             **earlystopping_kwargs):
 
-        self.l2_reg = l2_reg
-
-        use_cython_sampler = True
-
-        if self.RECOMMENDER_NAME == "PyTorchMF_BPR_Recommender":
-            data_iterator_class = BPRIterator_cython if use_cython_sampler else BPRIterator
-            self._data_iterator = data_iterator_class(URM_train=self.URM_train, batch_size=batch_size)
-        elif self.RECOMMENDER_NAME == "PyTorchMF_MSE_Recommender":
-            data_iterator_class = InteractionIterator_cython if use_cython_sampler else InteractionIterator
-            self._data_iterator = data_iterator_class(URM_train=self.URM_train, positive_quota=self.positive_quota,
-                                                      batch_size=batch_size)
-        else:
-            self._data_iterator = None
-
-        self._model = _SimpleMFModel(self.n_users, self.n_items, embedding_dim=num_factors)
-        self._model.to("cuda")
+        self._data_loader = DataLoader(self._dataset, batch_size = int(batch_size), shuffle = True, num_workers = os.cpu_count(), pin_memory=True)
+        self._model = _SimpleMFBiasModel(self.n_users, self.n_items, embedding_dim = num_factors)
+        # self._model.to("cuda")
 
         if sgd_mode.lower() == "adagrad":
-            self._optimizer = torch.optim.Adagrad(self._model.parameters(), lr=learning_rate)  # , weight_decay=l2_reg)
+            self._optimizer = torch.optim.Adagrad(self._model.parameters(), lr = learning_rate, weight_decay = l2_reg)
         elif sgd_mode.lower() == "rmsprop":
-            self._optimizer = torch.optim.RMSprop(self._model.parameters(), lr=learning_rate)  # , weight_decay=l2_reg)
+            self._optimizer = torch.optim.RMSprop(self._model.parameters(), lr = learning_rate, weight_decay = l2_reg)
         elif sgd_mode.lower() == "adam":
-            self._optimizer = torch.optim.Adam(self._model.parameters(), lr=learning_rate)  # , weight_decay=l2_reg)
+            self._optimizer = torch.optim.Adam(self._model.parameters(), lr = learning_rate, weight_decay = l2_reg)
         elif sgd_mode.lower() == "sgd":
-            self._optimizer = torch.optim.SGD(self._model.parameters(), lr=learning_rate)  # , weight_decay=l2_reg)
+            self._optimizer = torch.optim.SGD(self._model.parameters(), lr = learning_rate, weight_decay = l2_reg)
         else:
             raise ValueError("sgd_mode attribute value not recognized.")
+
 
         self._update_best_model()
 
         # with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], profile_memory=True, record_shapes=True) as prof:
 
         self._train_with_early_stopping(epochs,
-                                        algorithm_name=self.RECOMMENDER_NAME,
+                                        algorithm_name = self.RECOMMENDER_NAME,
                                         **earlystopping_kwargs)
 
         # print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=30))
@@ -280,23 +240,27 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
         self.USER_factors = self.USER_factors_best.copy()
         self.ITEM_factors = self.ITEM_factors_best.copy()
 
+
+
     def _prepare_model_for_validation(self):
         self.USER_factors = self._model._embedding_user.weight.detach().cpu().numpy()
         self.ITEM_factors = self._model._embedding_item.weight.detach().cpu().numpy()
+
 
     def _update_best_model(self):
         self.USER_factors_best = self._model._embedding_user.weight.detach().cpu().numpy()
         self.ITEM_factors_best = self._model._embedding_item.weight.detach().cpu().numpy()
 
+
     def _run_epoch(self, num_epoch):
 
         epoch_loss = 0
+        for batch in self._data_loader:
 
-        for batch in self._data_iterator:
             # Clear previously computed gradients
             self._optimizer.zero_grad()
 
-            loss = self._loss_function(self._model, batch, self.l2_reg)
+            loss = self._loss_function(self._model, batch)
 
             # Compute gradients given current loss
             loss.backward()
@@ -306,41 +270,44 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
 
             epoch_loss += loss.item()
 
-        self._print("Loss {:.2E}".format(epoch_loss))
 
 
 class PyTorchMF_BPR_Recommender(_PyTorchMFRecommender):
+
     RECOMMENDER_NAME = "PyTorchMF_BPR_Recommender"
 
-    def __init__(self, URM_train, verbose=True):
-        super(PyTorchMF_BPR_Recommender, self).__init__(URM_train, verbose=verbose)
+    def __init__(self, URM_train, verbose = True):
+        super(PyTorchMF_BPR_Recommender, self).__init__(URM_train, verbose = verbose)
 
+        self._dataset = BPR_Dataset(self.URM_train)
         self._loss_function = loss_BPR
 
 
 class PyTorchMF_MSE_Recommender(_PyTorchMFRecommender):
+
     RECOMMENDER_NAME = "PyTorchMF_MSE_Recommender"
 
-    def __init__(self, URM_train, verbose=True):
-        super(PyTorchMF_MSE_Recommender, self).__init__(URM_train, verbose=verbose)
+    def __init__(self, URM_train, verbose = True):
+        super(PyTorchMF_MSE_Recommender, self).__init__(URM_train, verbose = verbose)
 
-        self.positive_quota = None
+        self._dataset = None
         self._loss_function = loss_MSE
 
-    def fit(self, positive_quota=0.5, **kwargs):
-        self.positive_quota = positive_quota
+    def fit(self, positive_quota = 0.5, **kwargs):
+        self._dataset = Interaction_Dataset(self.URM_train, positive_quota = positive_quota)
         super(PyTorchMF_MSE_Recommender, self).fit(**kwargs)
 
 
 class PyTorchMF_Interaction_Recommender(_PyTorchMFRecommender):
+
     RECOMMENDER_NAME = "PyTorchMF_Interaction_Recommender"
 
-    def __init__(self, URM_train, verbose=True):
-        super(PyTorchMF_Interaction_Recommender, self).__init__(URM_train, verbose=verbose)
+    def __init__(self, URM_train, verbose = True):
+        super(PyTorchMF_Interaction_Recommender, self).__init__(URM_train, verbose = verbose)
 
         self._dataset = None
         self._loss_function = loss_CrossEntropy
 
-    def fit(self, positive_quota=0.5, **kwargs):
-        self._dataset = Interaction_Dataset(self.URM_train, positive_quota=positive_quota)
+    def fit(self, positive_quota = 0.5, **kwargs):
+        self._dataset = Interaction_Dataset(self.URM_train, positive_quota = positive_quota)
         super(PyTorchMF_Interaction_Recommender, self).fit(**kwargs)
