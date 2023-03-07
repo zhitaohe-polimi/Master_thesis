9f04e9c4f08ee0d8d8d81845880058a5b4d64d4e
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-07 13:32:40 +0100

    Update run_baseline_RecommenderWrapper.py

--- run_baseline_RecommenderWrapper.py
+++ run_baseline_RecommenderWrapper.py
@@ -223,21 +223,6 @@ def read_data_split_and_search(args):
         multiprocessing.set_start_method('spawn')
         pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count()), maxtasksperchild=1)
         pool.map(runParameterSearch_Collaborative_partial, recommender_class_list)
-        # runHyperparameterSearch_Collaborative(PyTorchMF_BPR_Recommender, #PyTorchNewMF_MSE_Recommender
-        #                                       URM_train=URM_train,
-        #                                       URM_train_last_test=URM_train_original,
-        #                                       metric_to_optimize=metric_to_optimize,
-        #                                       cutoff_to_optimize=cutoff_to_optimize,
-        #                                       n_cases=n_cases,
-        #                                       n_random_starts=int(n_cases / 3),
-        #                                       evaluator_validation_earlystopping=evaluator_validation,
-        #                                       evaluator_validation=evaluator_validation,
-        #                                       evaluate_on_test='best',
-        #                                       evaluator_test=evaluator_test,
-        #                                       output_folder_path=model_folder_path,
-        #                                       resume_from_saved=True,
-        #                                       similarity_type_list=None,  # all
-        #                                       parallelizeKNN=False)
 
 
 if __name__ == "__main__":
