1504f218a3093f698747bb4ea64089139bdfcffb
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-08 12:18:23 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -75,7 +75,7 @@ class _SimpleNewMFModel(torch.nn.Module):
         # user_sim_uv = torch.einsum("bi,ci->bc", self._embedding_user(user), self._embedding_user.weight)
         user_sim_uv = torch.corrcoef(self._embedding_user.weight.detach().cpu())[user.detach().cpu()]
         user_sim_uv[:, user] = user_sim_uv[:, user].fill_diagonal_(0)
-        user_sim_uv = torch.nn.functional.normalize(user_sim_uv, dim=1).to(self.device)
+        user_sim_uv = torch.nn.functional.normalize(user_sim_uv, dim=1).to("cuda")
         alpha_vi = torch.einsum("bi,ci->bc", self._embedding_user_vi.weight, self._embedding_item_vi(item))
         summation_v = torch.einsum("bi,ib->b", user_sim_uv, alpha_vi)
         prediction += summation_v
@@ -83,7 +83,7 @@ class _SimpleNewMFModel(torch.nn.Module):
         # item_sim_ij = torch.einsum("bi,ci->bc", self._embedding_item.weight, self._embedding_item(item))
         item_sim_ij = torch.corrcoef(self._embedding_item.weight.detach().cpu())[:, item.detach().cpu()]
         item_sim_ij[item] = item_sim_ij[item].fill_diagonal_(0)
-        item_sim_ij = torch.nn.functional.normalize(item_sim_ij, dim=0)
+        item_sim_ij = torch.nn.functional.normalize(item_sim_ij, dim=0).to("cuda")
         alpha_uj = torch.einsum("bi,ci->bc", self._embedding_user_uj(user), self._embedding_item_uj.weight)
         summation_j = torch.einsum("bi,ib->b", alpha_uj, item_sim_ij)
         prediction += summation_j
