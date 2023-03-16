a4b985a4cc7882a8369935b3fcc4a9e15e628de9
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-16 17:15:40 +0100

    Update run_baseline_RecommenderWrapper.py

--- run_baseline_RecommenderWrapper.py
+++ run_baseline_RecommenderWrapper.py
@@ -189,7 +189,7 @@ def read_data_split_and_search(args):
     if args.flag_baselines_tune:
         recommender_class_list = [
             # P3alphaRecommender,
-            SLIM_BPR_Cython,
+            # SLIM_BPR_Cython,
             # MatrixFactorization_BPR_Cython,
             # IALSRecommender,
             # MatrixFactorization_FunkSVD_Cython,
@@ -203,7 +203,7 @@ def read_data_split_and_search(args):
             # ItemKNNCBFRecommender,
             # new_MatrixFactorization_FunkSVD_Cython,
             # PyTorchNewMF_MSE_Recommender,
-            # PyTorchNewMF_BPR_Recommender,
+            PyTorchNewMF_BPR_Recommender,
             # PyTorchMF_BPR_Recommender,
             # PyTorchMF_MSE_Recommender
         ]
@@ -242,7 +242,7 @@ if __name__ == "__main__":
                         help='Project path.')
 
     parser.add_argument('--dataset', help="Choose a dataset from {yelp2018, last-fm, amazon-book}",
-                        default="amazon-book")
+                        default="movie-lens")
 
     parser.add_argument('--pretrain', type=int, default=-1,
                         help='0: No pretrain, -1: Pretrain with the learned embeddings, 1:Pretrain with stored models.')
