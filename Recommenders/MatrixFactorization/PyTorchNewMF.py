73ff4090c67bb0a6d04782780e79fb7a67c22bae
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-20 18:55:20 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -323,15 +323,15 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
         :return:
         """
 
-        assert self.USER_factors.weight.shape[1] == self.ITEM_factors.weight.shape[1], \
+        assert self.USER_factors.shape[1] == self.ITEM_factors.shape[1], \
             "{}: User and Item factors have inconsistent shape".format(self.RECOMMENDER_NAME)
 
-        assert self.USER_factors.weight.shape[0] > np.max(user_id_array), \
+        assert self.USER_factors.shape[0] > np.max(user_id_array), \
             "{}: Cold users not allowed. Users in trained model are {}, requested prediction for users up to {}".format(
-                self.RECOMMENDER_NAME, self.USER_factors.weight.shape[0], np.max(user_id_array))
+                self.RECOMMENDER_NAME, self.USER_factors.shape[0], np.max(user_id_array))
 
         user_id_array = torch.LongTensor(user_id_array)#.to(self.device)
-        user_id_array = torch.Tensor(user_id_array).type(torch.LongTensor)#.to(self.device)
+        # user_id_array = torch.Tensor(user_id_array).type(torch.LongTensor)#.to(self.device)
         USER_factors = torch.tensor(self.USER_factors)#.to(self.device)
         ITEM_factors = torch.tensor(self.ITEM_factors)#.to(self.device)
         USER_factors_vi = torch.tensor(self.USER_factors_vi)#.to(self.device)
@@ -371,10 +371,10 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
             # item_scores += summation_j
             # item_scores = item_scores.detach().cpu().numpy()
 
-            n_items = ITEM_factors.weight.shape[0]
+            n_items = ITEM_factors.shape[0]
             interval = len(user_id_array)
-            item_id_list = torch.LongTensor(range(n_items)).to(self.device)
-            item_scores = - np.ones((len(user_id_array), ITEM_factors.weight.shape[0]), dtype=np.float32) * np.inf
+            item_id_list = torch.LongTensor(range(n_items))#.to(self.device)
+            item_scores = - np.ones((len(user_id_array), ITEM_factors.shape[0]), dtype=np.float32) * np.inf
             n_sampled_intervals = 0
             for i in range(0, math.ceil(n_items / interval)):
                 print("%d:%d" % (n_sampled_intervals * interval, min((n_sampled_intervals + 1) * interval, n_items)))
