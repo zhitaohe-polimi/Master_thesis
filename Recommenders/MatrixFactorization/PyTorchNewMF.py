1db81ff5d03bb7ab58cc51272c503c03987173c9
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-20 15:51:46 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -362,20 +362,18 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
 
         else:
             item_scores = torch.einsum("bi,ci->bc", USER_factors[user_id_array], ITEM_factors)
-            # user_sim_uv = torch.einsum("bi,ci->bc", USER_factors[user_id_array], USER_factors)
             user_sim_uv = pearson_corr(USER_factors[user_id_array], USER_factors)
             user_sim_uv[:, user_id_array] = user_sim_uv[:, user_id_array].fill_diagonal_(0)
             user_sim_uv = torch.nn.functional.normalize(user_sim_uv, p=1, dim=1)
             alpha_vi = torch.einsum("bi,ci->bc", USER_factors_vi, ITEM_factors_vi)
-            alpha_vi = rescaling_new(alpha_vi, 0)
+            alpha_vi = rescaling(alpha_vi, 0)
             summation_v = torch.einsum("bi,ic->bc", user_sim_uv, alpha_vi)
             item_scores += summation_v
 
-            # item_sim_ij = torch.einsum("bi,ci->bc", ITEM_factors, ITEM_factors).fill_diagonal_(0)
             item_sim_ij = pearson_corr(ITEM_factors, ITEM_factors).fill_diagonal_(0)
             item_sim_ij = torch.nn.functional.normalize(item_sim_ij, p=1, dim=0)
             alpha_uj = torch.einsum("bi,ci->bc", USER_factors_uj[user_id_array], ITEM_factors_uj)
-            alpha_uj = rescaling_new(alpha_uj, 1)
+            alpha_uj = rescaling(alpha_uj, 1)
             summation_j = torch.einsum("bi,ic->bc", alpha_uj, item_sim_ij)
             item_scores += summation_j
             item_scores = item_scores.detach().cpu().numpy()
