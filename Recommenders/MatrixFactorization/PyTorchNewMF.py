77265078f72267260cb9a11fb650fe7b2d800aad
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-20 18:59:14 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -381,19 +381,19 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
                 items_to_compute = item_id_list[
                                    n_sampled_intervals * interval:min((n_sampled_intervals + 1) * interval, n_items)]
 
-                predictions = torch.einsum("bi,ci->bc", USER_factors(user_id_array), ITEM_factors(items_to_compute))
-                user_sim_uv = pearson_corr(USER_factors(user_id_array), USER_factors.weight)
+                predictions = torch.einsum("bi,ci->bc", USER_factors[user_id_array], ITEM_factors[items_to_compute])
+                user_sim_uv = pearson_corr(USER_factors[user_id_array], USER_factors.weight)
                 user_sim_uv[:, user_id_array] = user_sim_uv[:, user_id_array].fill_diagonal_(0)
                 user_sim_uv = torch.nn.functional.normalize(user_sim_uv, p=1, dim=1)
-                alpha_vi = torch.einsum("bi,ci->bc", USER_factors_vi.weight, ITEM_factors_vi(items_to_compute))
+                alpha_vi = torch.einsum("bi,ci->bc", USER_factors_vi.weight, ITEM_factors_vi[items_to_compute])
                 alpha_vi = rescaling(alpha_vi, 0)
                 summation_v = torch.einsum("bi,ic->bc", user_sim_uv, alpha_vi)
                 predictions += summation_v
 
-                item_sim_ij = pearson_corr(ITEM_factors.weight, ITEM_factors(items_to_compute))
+                item_sim_ij = pearson_corr(ITEM_factors.weight, ITEM_factors[items_to_compute])
                 item_sim_ij[items_to_compute] = item_sim_ij[items_to_compute].fill_diagonal_(0)
                 item_sim_ij = torch.nn.functional.normalize(item_sim_ij, p=1, dim=0)
-                alpha_uj = torch.einsum("bi,ci->bc", USER_factors_uj(user_id_array), ITEM_factors_uj.weight)
+                alpha_uj = torch.einsum("bi,ci->bc", USER_factors_uj[user_id_array], ITEM_factors_uj.weight)
                 alpha_uj = rescaling(alpha_uj, 1)
                 summation_j = torch.einsum("bi,ic->bc", alpha_uj, item_sim_ij)
                 predictions += summation_j
