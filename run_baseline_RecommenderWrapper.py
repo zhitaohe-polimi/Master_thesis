eb9d85be4cc7518cae1cc25a64b64a166c2de06e
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-24 10:16:21 +0100

    Update run_baseline_RecommenderWrapper.py

--- run_baseline_RecommenderWrapper.py
+++ run_baseline_RecommenderWrapper.py
@@ -190,7 +190,7 @@ def read_data_split_and_search(args):
         recommender_class_list = [
             # P3alphaRecommender,
             # SLIM_BPR_Cython,
-            MatrixFactorization_BPR_Cython,
+            # MatrixFactorization_BPR_Cython,
             # IALSRecommender,
             MatrixFactorization_FunkSVD_Cython,
             # MatrixFactorization_AsySVD_Cython,
