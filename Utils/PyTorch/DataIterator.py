9e94dcaae8b574e239fffd2cf483da1c4d854211
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-24 13:58:30 +0100

    Update DataIterator.py

--- Utils/PyTorch/DataIterator.py
+++ Utils/PyTorch/DataIterator.py
@@ -256,7 +256,7 @@ class BPRIterator(object):
         for i_batch in range(0, min(self.batch_size, self.n_data_points-self.n_sampled_points)):
 
             self.n_sampled_points +=1
-            index = np.random.randint(self.n_users)
+            index = np.random.randint(self.n_users-len([np.ediff1d(self.URM_train.indptr) > 0])-1)
             user_id = self.warm_user_index_to_original_id[index]
 
             start_pos_seen_items = self.URM_train.indptr[user_id]
