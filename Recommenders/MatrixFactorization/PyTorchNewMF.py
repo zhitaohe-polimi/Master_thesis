b26b2118096ca36962bd6fe120e2213f352d282f
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-25 18:56:28 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -269,7 +269,7 @@ def calculate_prediction(user_id_array, items_to_compute, USER_factors, ITEM_fac
     user_sim_uv[:, user_id_array] = user_sim_uv[:, user_id_array].fill_diagonal_(0)
     user_sim_uv = torch.nn.functional.normalize(user_sim_uv, p=1, dim=1)
     alpha_vi = torch.einsum("bi,ci->bc", USER_factors_vi, ITEM_factors_vi[items_to_compute])
-    alpha_vi = rescaling(alpha_vi, 0)
+    # alpha_vi = rescaling(alpha_vi, 0)
     summation_v = torch.einsum("bi,ic->bc", user_sim_uv, alpha_vi)
     prediction += summation_v
 
@@ -277,7 +277,7 @@ def calculate_prediction(user_id_array, items_to_compute, USER_factors, ITEM_fac
     item_sim_ij[items_to_compute] = item_sim_ij[items_to_compute].fill_diagonal_(0)
     item_sim_ij = torch.nn.functional.normalize(item_sim_ij, p=1, dim=0)
     alpha_uj = torch.einsum("bi,ci->bc", USER_factors_uj[user_id_array], ITEM_factors_uj)
-    alpha_uj = rescaling(alpha_uj, 1)
+    # alpha_uj = rescaling(alpha_uj, 1)
     summation_j = torch.einsum("bi,ic->bc", alpha_uj, item_sim_ij)
     prediction += summation_j
 
