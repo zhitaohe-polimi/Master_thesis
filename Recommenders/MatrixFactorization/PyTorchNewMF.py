49c0120bfb8a5c9fd5dbfff5fd17facf15c9e822
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-03-25 20:13:11 +0100

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -465,7 +465,7 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
         epoch_loss = 0
         summ = 0
         tracemalloc.start() #_data_iterator
-        for batch in self._data_loader:
+        for batch in self._data_iterator:
             summ += len(batch[0])
 
             # Clear previously computed gradients
