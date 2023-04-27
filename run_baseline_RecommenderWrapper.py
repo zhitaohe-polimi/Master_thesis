3532e7f8f8fd2b9e940f96617191e48f5ba8cd43
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-04-27 16:42:08 +0200

    Update run_baseline_RecommenderWrapper.py

--- run_baseline_RecommenderWrapper.py
+++ run_baseline_RecommenderWrapper.py
@@ -208,7 +208,7 @@ def read_data_split_and_search(args):
             # PyTorchMF_MSE_Recommender
         ]
 
-        n_cases = 10
+        n_cases = 5
 
         runParameterSearch_Collaborative_partial = partial(runHyperparameterSearch_Collaborative,
                                                            URM_train=URM_train,
