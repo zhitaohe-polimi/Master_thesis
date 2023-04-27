466c4c049387e44f43c65a97ca8667da5a7508e4
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-04-27 13:35:34 +0200

    Update run_recommendation_result.py

--- run_recommendation_result.py
+++ run_recommendation_result.py
@@ -17,7 +17,7 @@ from Recommenders.SLIM.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 
 
 if __name__ == '__main__':
-    dataset_name = "yelp2018"
+    dataset_name = "last-fm"
     dataset_path = '/home/tesista/Master_thesis/Conferences/HGB/HGB_github/baseline/Data/'
 
     if dataset_name == "movie-lens" or dataset_name == "last-fm" or dataset_name == "yelp2018" or dataset_name == "amazon-book":
