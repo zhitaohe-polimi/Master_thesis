bd584cebd32d317300df868262460183d768bbdf
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-04-27 13:09:47 +0200

    Update run_recommendation_result.py

--- run_recommendation_result.py
+++ run_recommendation_result.py
@@ -29,62 +29,84 @@ if __name__ == '__main__':
 
     URM_submission_train = URM_train+URM_validation
 
-    recommender_class_list = [
-        P3alphaRecommender,
-        SLIM_BPR_Cython,
-        MatrixFactorization_BPR_Cython,
-        IALSRecommender,
-        MatrixFactorization_FunkSVD_Cython,
-        MatrixFactorization_AsySVD_Cython,
-        # ItemKNNCFRecommender,
-        # UserKNNCFRecommender,
-        PureSVDRecommender,
-        PyTorchNewMF_BPR_Recommender,
-        PyTorchMF_BPR_Recommender,
-    ]
-
-    for rec in recommender_class_list:
-        try:
-            rec = rec(URM_submission_train)
-            rec.load_model(
-                folder_path='result_experiments/baseline/{}/models_RECALL/'.format(dataset_name),
-                file_name='{}_best_model_last.zip'.format(rec.RECOMMENDER_NAME))
-    #UserKNNCFRecommender_jaccard
-
-    # rec = UserKNNCFRecommender(URM_submission_train)
-    # rec.load_model(
-    #     folder_path='result_experiments/baseline/{}/models_RECALL/'.format(dataset_name),
-    #     file_name='UserKNNCFRecommender_jaccard_best_model_last.zip')
-
-    # ######## ignore out of stock
-    # df_articles = pd.read_parquet('{}/processed_articles.parquet'.format(DATASET_PATH))
-    # df_article_out_of_stock = df_articles.query("out_of_stock==1")[
-    #     ['article_id', 'out_of_stock']]  # ['article_id'].unique().tolist()
+    # recommender_class_list = [
+    #     P3alphaRecommender,
+    #     SLIM_BPR_Cython,
+    #     MatrixFactorization_BPR_Cython,
+    #     IALSRecommender,
+    #     MatrixFactorization_FunkSVD_Cython,
+    #     MatrixFactorization_AsySVD_Cython,
+    #     # ItemKNNCFRecommender,
+    #     # UserKNNCFRecommender,
+    #     PureSVDRecommender,
+    #     PyTorchNewMF_BPR_Recommender,
+    #     PyTorchMF_BPR_Recommender,
+    # ]
     #
-    # item_original_ID_to_index_mapper = dataset.get_item_original_ID_to_index_mapper()
-    # df_article_out_of_stock['article_id_index'] = df_article_out_of_stock.apply(
-    #     lambda x: item_original_ID_to_index_mapper[x.article_id], axis=1)
+    # for rec in recommender_class_list:
+    #     try:
+    #         rec = rec(URM_submission_train)
+    #         rec.load_model(
+    #             folder_path='result_experiments/baseline/{}/models_RECALL/'.format(dataset_name),
+    #             file_name='{}_best_model_last.zip'.format(rec.RECOMMENDER_NAME))
+    # #UserKNNCFRecommender_jaccard
     #
-    # print(df_article_out_of_stock)
+    # # rec = UserKNNCFRecommender(URM_submission_train)
+    # # rec.load_model(
+    # #     folder_path='result_experiments/baseline/{}/models_RECALL/'.format(dataset_name),
+    # #     file_name='UserKNNCFRecommender_jaccard_best_model_last.zip')
     #
-    # out_of_stock_list = df_article_out_of_stock['article_id_index'].unique().tolist()
-    # recommender.set_items_to_ignore(out_of_stock_list)
+    # # ######## ignore out of stock
+    # # df_articles = pd.read_parquet('{}/processed_articles.parquet'.format(DATASET_PATH))
+    # # df_article_out_of_stock = df_articles.query("out_of_stock==1")[
+    # #     ['article_id', 'out_of_stock']]  # ['article_id'].unique().tolist()
+    # #
+    # # item_original_ID_to_index_mapper = dataset.get_item_original_ID_to_index_mapper()
+    # # df_article_out_of_stock['article_id_index'] = df_article_out_of_stock.apply(
+    # #     lambda x: item_original_ID_to_index_mapper[x.article_id], axis=1)
+    # #
+    # # print(df_article_out_of_stock)
+    # #
+    # # out_of_stock_list = df_article_out_of_stock['article_id_index'].unique().tolist()
+    # # recommender.set_items_to_ignore(out_of_stock_list)
+    # #
+    # # #####################
     #
-    # #####################
+    #         path = "result_experiments/{}/{}/".format('baseline', dataset_name)
+    #
+    #         save_path = os.path.join(path, "{}-recommendation_results.csv".format(dataset_name+"_"+rec.RECOMMENDER_NAME))
+    #
+    #         f = open(save_path, "w")
+    #         f.write("customer_id,prediction\n")
+    #
+    #         for i in range(URM_submission_train.shape[0]):
+    #             recommended_items = rec.recommend(i, cutoff=20, remove_seen_flag=False)
+    #             well_formatted = str(i)+","+" ".join([str(x) for x in recommended_items])
+    #             f.write(f"{i}, {well_formatted}\n")
+    #             print("%s:%s" % (i, well_formatted))
+    #         f.close()
+    #         print("save complete")
+    #     except Exception as e:
+    #         print(e)
 
-            path = "result_experiments/{}/{}/".format('baseline', dataset_name)
+    rec = ItemKNNCFRecommender(URM_submission_train)
+    rec.load_model(
+        folder_path='result_experiments/baseline/{}/models_RECALL/'.format(dataset_name),
+        file_name='ItemKNNCFRecommender_asymmetric_best_model_last.zip')
 
-            save_path = os.path.join(path, "{}-recommendation_results.csv".format(dataset_name+"_"+rec.RECOMMENDER_NAME))
+    path = "result_experiments/{}/{}/".format('baseline', dataset_name)
 
-            f = open(save_path, "w")
-            f.write("customer_id,prediction\n")
+    save_path = os.path.join(path, "{}-recommendation_results.csv".format(dataset_name+"_"+rec.RECOMMENDER_NAME))
 
-            for i in range(URM_submission_train.shape[0]):
-                recommended_items = rec.recommend(i, cutoff=20, remove_seen_flag=False)
-                well_formatted = str(i)+","+" ".join([str(x) for x in recommended_items])
-                f.write(f"{i}, {well_formatted}\n")
-                print("%s:%s" % (i, well_formatted))
-            f.close()
-            print("save complete")
-        except Exception as e:
-            print(e)
+    f = open(save_path, "w")
+    f.write("customer_id,prediction\n")
+
+    for i in range(URM_submission_train.shape[0]):
+        recommended_items = rec.recommend(i, cutoff=20, remove_seen_flag=False)
+        well_formatted = str(i)+","+" ".join([str(x) for x in recommended_items])
+        f.write(f"{i}, {well_formatted}\n")
+        print("%s:%s" % (i, well_formatted))
+    f.close()
+    print("save complete")
+except Exception as e:
+    print(e)
