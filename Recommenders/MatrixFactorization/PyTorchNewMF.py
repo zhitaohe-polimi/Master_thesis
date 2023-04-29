0e239b4c6d16412b4e4a4e6c608867f8b5a095be
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-04-29 08:39:54 +0200

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -356,8 +356,7 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
 
         else:
             n_items = ITEM_factors.shape[0]
-            interval = round(n_items / 2, 0)  # len(user_id_array)
-            print(interval)
+            interval = int(round(n_items / 2, 0))  # len(user_id_array)
             item_id_list = torch.LongTensor(range(n_items)).to(self.device)
             item_scores = - np.ones((len(user_id_array), ITEM_factors.shape[0]), dtype=np.float32) * np.inf
             item_scores = torch.from_numpy(item_scores).to(self.device)
