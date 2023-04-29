ac93c47a9868f154dc9522ea11dc9086b5ffe0d4
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-04-29 08:25:17 +0200

    Update PyTorchNewMF.py

--- Recommenders/MatrixFactorization/PyTorchNewMF.py
+++ Recommenders/MatrixFactorization/PyTorchNewMF.py
@@ -356,32 +356,32 @@ class _PyTorchMFRecommender(BaseMatrixFactorizationRecommender, Incremental_Trai
 
         else:
             n_items = ITEM_factors.shape[0]
-            interval = len(user_id_array)
+            # interval = len(user_id_array)
             item_id_list = torch.LongTensor(range(n_items)).to(self.device)
-            item_scores = - np.ones((len(user_id_array), ITEM_factors.shape[0]), dtype=np.float32) * np.inf
-            item_scores = torch.from_numpy(item_scores).to(self.device)
-            n_sampled_intervals = 0
-            for i in range(0, math.ceil(n_items / interval)):
-                # print("%d:%d" % (n_sampled_intervals * interval, min((n_sampled_intervals + 1) * interval, n_items)))
-                items_id_array = item_id_list[
-                                 n_sampled_intervals * interval:min((n_sampled_intervals + 1) * interval, n_items)]
-
-                predictions = calculate_prediction(user_id_array, items_id_array, USER_factors, ITEM_factors,
+            # item_scores = - np.ones((len(user_id_array), ITEM_factors.shape[0]), dtype=np.float32) * np.inf
+            # item_scores = torch.from_numpy(item_scores).to(self.device)
+            # n_sampled_intervals = 0
+            # for i in range(0, math.ceil(n_items / interval)):
+            #     # print("%d:%d" % (n_sampled_intervals * interval, min((n_sampled_intervals + 1) * interval, n_items)))
+            #     items_id_array = item_id_list[
+            #                      n_sampled_intervals * interval:min((n_sampled_intervals + 1) * interval, n_items)]
+            #
+            #     predictions = calculate_prediction(user_id_array, items_id_array, USER_factors, ITEM_factors,
+            #                                        USER_factors_vi, ITEM_factors_vi,
+            #                                        USER_factors_uj, ITEM_factors_uj)
+            #
+            #     item_scores[:, items_id_array] = predictions
+            #
+            #     n_sampled_intervals += 1
+            #
+            predictions = calculate_prediction(user_id_array, item_id_list, USER_factors, ITEM_factors,
                                                    USER_factors_vi, ITEM_factors_vi,
                                                    USER_factors_uj, ITEM_factors_uj)
 
-                item_scores[:, items_id_array] = predictions
 
-                n_sampled_intervals += 1
-            #
-            # predictions = calculate_prediction(user_id_array, item_id_list, USER_factors, ITEM_factors,
-            #                                        USER_factors_vi, ITEM_factors_vi,
-            #                                        USER_factors_uj, ITEM_factors_uj)
+            # item_scores = item_scores.detach().cpu().numpy()
 
-
-            item_scores = item_scores.detach().cpu().numpy()
-
-            # item_scores = predictions.detach().cpu().numpy()
+            item_scores = predictions.detach().cpu().numpy()
         # No need to select only the specific negative items or warm users because the -inf score will not change
         if self.use_bias:
             item_scores += self.ITEM_bias + self.GLOBAL_bias
