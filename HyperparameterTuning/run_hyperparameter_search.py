f41b30df3976cd1cefc2a059a5561d0cd174c272
zhitaohe-polimi <zhitao.he@mail.polimi.it>
2023-05-09 08:35:32 +0200

    Update run_hyperparameter_search.py

--- HyperparameterTuning/run_hyperparameter_search.py
+++ HyperparameterTuning/run_hyperparameter_search.py
@@ -845,7 +845,7 @@ def runHyperparameterSearch_Collaborative(recommender_class, URM_train, URM_trai
 
         if recommender_class is PyTorchNewMF_BPR_Recommender:
             hyperparameters_range_dictionary = {
-                "num_factors": Categorical([1]),  # Integer(1, 200), Categorical([186])
+                "num_factors": Categorical([2]),  # Integer(1, 200), Categorical([186])
                 "num_factors_u": Categorical([162]),  # Integer(1, 200), Categorical([188])
                 "num_factors_i": Categorical([14]),  # Integer(1, 200), Categorical([1])
                 "epochs": Categorical([1500]),
